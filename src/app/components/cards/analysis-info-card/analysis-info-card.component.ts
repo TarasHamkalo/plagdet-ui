f786d0afbbaf9d33df22fe11e7a7486a7a3a5245
Taras Hamkalo <hamkalo.taras@gmail.com>
2025-03-02 16:07:46 +0100

    refactor: normalize model

--- src/app/components/cards/analysis-info-card/analysis-info-card.component.ts
+++ src/app/components/cards/analysis-info-card/analysis-info-card.component.ts
@@ -1,8 +1,7 @@
-import {Component, effect, Input, signal} from "@angular/core";
+import {Component, computed, Input} from "@angular/core";
 import {MatIcon} from "@angular/material/icon";
 import {MatList, MatListItem, MatListItemIcon, MatListItemTitle} from "@angular/material/list";
 import {MatDivider} from "@angular/material/divider";
-import {Overview} from "../../../model/overview";
 import {DatePipe, DecimalPipe} from "@angular/common";
 import {MatChip} from "@angular/material/chips";
 import {MatTooltip} from "@angular/material/tooltip";
@@ -27,18 +26,31 @@ import {AnalysisContextService} from "../../../context/analysis-context.service"
 })
 export class AnalysisInfoCardComponent {
 
-  @Input() public supportiveText = "V tejto časti sa zobrazujú informácie o analýze a o tom, aké parametre sa pri nej použili";
+  @Input() public supportiveText = "V tejto časti sa zobrazujú informácie o analýze";
 
-  public analysis = signal<Overview | undefined>(undefined);
+  protected analysis = computed(() => {
+    const report = this.analysisContext.getReport()();
+    if (report) {
+      return report!.overview;
+    }
+    return null;
+  });
+
+  protected extensionToCount = computed(() => {
+    const report = this.analysisContext.getReport()();
+    if (report) {
+      return Array.from(report.submissions.values())
+        .map(submission => submission.fileData.extension)
+        .reduce((acc, ext) => {
+          acc.set(ext, (acc.get(ext) || 0) + 1);
+          return acc;
+        }, new Map<string, number>());
+    }
+
+    return new Map<string, number>();
+  });
 
   constructor(private analysisContext: AnalysisContextService) {
-    effect(() => {
-      const report = this.analysisContext.getReport()();
-      if (report) {
-        // report.submissions.
-        this.analysis.set(report!.overview);
-      }
-    });
   }
 
 }
--- src/app/model/overview.ts
+++ src/app/model/overview.ts
@@ -5,10 +5,6 @@ export interface Overview {
   name: string;
   analyzedFileName?: string;
   dateAnalyzed: Date;
-  numberOfComparisons: number;
-  docxFiles: number;
-  docFiles: number;
-  pdfFiles: number;
-  totalFilesCount: number; // shouldn't be there (but for prototype simplifies table logic)
+  comparisonsCount: number;
   configuration: PlagdetConfiguration;
 }
