74553ff0c1ca86aa7d1a86e582ab458fc633ad9c
Taras Hamkalo <hamkalo.taras@gmail.com>
2025-03-14 18:26:15 +0100

    feat: add navigation item description

--- src/app/components/navigation/nav-item-view/nav-item-view.component.ts
+++ src/app/components/navigation/nav-item-view/nav-item-view.component.ts
@@ -1,19 +1,20 @@
 import {Component, computed, Input, signal} from "@angular/core";
 import {NavItem} from "../../../types/nav-item";
-import {MatListItem} from "@angular/material/list";
 import {MatIcon} from "@angular/material/icon";
 import {NavigationService} from "../../../services/navigation/navigation.service";
-import {RouterLink} from "@angular/router";
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import {MatRipple} from "@angular/material/core";
 
 @Component({
   selector: "app-nav-item-view",
   imports: [
-    MatListItem,
     MatIcon,
-    RouterLink
+    RouterLink,
+    MatRipple,
+    RouterLinkActive
   ],
   templateUrl: "./nav-item-view.component.html",
-  styleUrl: "./nav-item-view.component.css"
+  styleUrl: "./nav-item-view.component.scss"
 })
 export class NavItemViewComponent {
 
