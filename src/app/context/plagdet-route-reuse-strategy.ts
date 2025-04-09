5bd9aa3c77058fa6f01842c258a46230c8f48c9f
Taras Hamkalo <hamkalo.taras@gmail.com>
2025-04-09 19:37:40 +0200

    fix: change caching scheme to full path instead of base

--- src/app/context/plagdet-route-reuse-strategy.ts
+++ src/app/context/plagdet-route-reuse-strategy.ts
@@ -6,13 +6,16 @@ export class PlagdetRouteReuseStrategy implements RouteReuseStrategy {
 
   protected allowRetrieveCache = new Map<string, boolean>([
     ["graph", true],
-    ["heatmap", false],
-    ["diff", true]
+    ["diff", true],
+    ["pairs", true]
   ]);
 
-  private getBaseRoute(route: ActivatedRouteSnapshot): string | undefined {
-    console.log("Route path:", route.routeConfig?.path);
-    return route.routeConfig?.path?.split("/")[0];
+  // private getBasePath(route: ActivatedRouteSnapshot): string | undefined {
+  //   return route.routeConfig?.path?.split("/")[0];
+  // }
+
+  private getBasePath(path: string): string {
+    return path.split("/").pop()!;
   }
 
   public shouldReuseRoute(before: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
@@ -20,45 +23,47 @@ export class PlagdetRouteReuseStrategy implements RouteReuseStrategy {
   }
 
   public retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle | null {
-    const basePath = this.getBaseRoute(route);
-    console.log(`Retrieve attach for: ${basePath}`);
-    return basePath && this.allowRetrieveCache.get(basePath) ? this.storedRouteHandles.get(basePath) || null : null;
+    const path = route.routeConfig?.path;
+    if (path && this.allowRetrieveCache.get(this.getBasePath(path))) {
+      const route = this.storedRouteHandles.get(path);
+      return route ? route : null;
+    }
+
+    return null;
   }
 
   public shouldAttach(route: ActivatedRouteSnapshot): boolean {
-    const basePath = this.getBaseRoute(route);
-    console.log(`Checking attach for: ${basePath}`);
-    if (basePath) {
-      const cacheAllowed = this.allowRetrieveCache.get(basePath!);
-      return (cacheAllowed !== undefined && cacheAllowed) && this.storedRouteHandles.has(basePath);
+    const path = route.routeConfig?.path;
+    if (path && this.allowRetrieveCache.get(this.getBasePath(path))) {
+      return this.storedRouteHandles.has(path);
     }
 
     return false;
   }
 
   public shouldDetach(route: ActivatedRouteSnapshot): boolean {
-    const basePath = this.getBaseRoute(route);
-    if (basePath) {
-      if (basePath === PageRoutes.HOME) {
+    const path = route.routeConfig?.path;
+    if (path) {
+      const basePath = this.getBasePath(path);
+      if (basePath === PageRoutes.HOME.substring(1)) {
         this.storedRouteHandles.clear();
         return false;
       }
 
-      const cacheAllowed = this.allowRetrieveCache.get(basePath);
-      return cacheAllowed != undefined && cacheAllowed;
+      if (basePath) {
+        const cacheAllowed = this.allowRetrieveCache.get(basePath);
+        return cacheAllowed != undefined && cacheAllowed;
+      }
     }
+
 
     return false;
   }
 
   public store(route: ActivatedRouteSnapshot, detachedTree: DetachedRouteHandle): void {
-    const basePath = this.getBaseRoute(route);
-    if (basePath) {
-      const cacheAllowed = this.allowRetrieveCache.get(basePath);
-      if (cacheAllowed != undefined && cacheAllowed) {
-        console.log(`Storing route: ${basePath}`);
-        this.storedRouteHandles.set(basePath, detachedTree);
-      }
+    const path = route.routeConfig?.path;
+    if (path && this.allowRetrieveCache.get(this.getBasePath(path))) {
+      this.storedRouteHandles.set(path, detachedTree);
     }
   }
 }
