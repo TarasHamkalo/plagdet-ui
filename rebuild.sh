20dc0e255521c64dace749f527748bcc9849af4c
Taras Hamkalo <hamkalo.taras@gmail.com>
2025-04-28 15:55:57 +0200

    chore: stable but can not load specific worker

---
+++ rebuild.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+rm -rf ./docs
+
+ng build --base-href /plagdet-ui/ --deploy-url /plagdet-ui/ --output-path docs --configuration production
+
+mv ./docs/browser/* ./docs
+
+rm -rf ./docs/browser
+
