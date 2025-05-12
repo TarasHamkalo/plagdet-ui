d41bbc2eb875902da09fa2b369ad4e5d73e1a50f
Taras Hamkalo <hamkalo.taras@gmail.com>
2025-05-12 15:43:33 +0200

    refactor: dynamically bind super nice monaco paths

--- docs/chunk-VPOIPSIV.js
+++
@@ -1,1 +0,0 @@
-import{a,b}from"./chunk-5FOUROUK.js";import"./chunk-72WQYDLT.js";import"./chunk-EJUJSVO6.js";export{a as conf,b as language};
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,8 +6,26 @@ import {provideAnimationsAsync} from "@angular/platform-browser/animations/async
 import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 import {MatPaginatorIntl} from "@angular/material/paginator";
 import {SlovakPaginatorService} from "./services/localization/slovak-paginator.service";
-import {provideMonacoEditor} from "ngx-monaco-editor-v2";
+import {NgxMonacoEditorConfig, provideMonacoEditor} from "ngx-monaco-editor-v2";
 import {PlagdetRouteReuseStrategy} from "./context/plagdet-route-reuse-strategy";
+
+(self as any).MonacoEnvironment = {
+  getWorkerUrl: function (moduleId: string, label: string) {
+    // @ts-expect-error token will be there!
+    return `${window["base-href"]}/assets/monaco/min/vs/base/worker/workerMain.js`;
+  }
+};
+
+const monacoConfig: NgxMonacoEditorConfig = {
+  requireConfig: {
+    preferScriptTags: true,
+    paths: {
+      // @ts-expect-error token will be there!
+      "vs": `${window["base-href"]}/assets/monaco/min/vs`
+    }
+  },
+  monacoRequire: (window as any).monacoRequire
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
